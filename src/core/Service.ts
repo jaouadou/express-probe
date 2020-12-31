ec0c4fd8587ac20d70615b262feb184e997a7ffe
emanuelosva <emanuelosva@gmail.com>
2020-12-31 09:06:51 -0600

    :wrench: log and fix query

--- src/core/Service.ts
+++ src/core/Service.ts
@@ -9,22 +9,28 @@
  */
 
 import { Request } from 'express'
-import { ApiError } from '../lib'
-import { Model } from 'mongoose'
-import { Cache } from './Cache'
+import { Model, Types } from 'mongoose'
+import { ApiError, Logger } from '../lib'
 import { GenericObject, GenericDocument } from '../interfaces'
+import { Cache } from '../services/cache'
+import config from '../config'
 
 export class Service {
-  public model: Model<GenericDocument>
+  private _model: Model<any>
+  public name: string
+  private paginationLimit: number
+  public logger: Logger
 
   constructor(
-    model: Model<GenericDocument>,
-    private cache: Cache,
-    private conf: { name: string, paginationLimit: number },
+    model: Model<any>,
+    protected cache: Cache,
+    conf: { name: string, paginationLimit?: number },
   ) {
-    this.model = model
+    this._model = model
     this.cache = cache
-    this.conf = conf
+    this.logger = new Logger(conf.name)
+    this.name = conf.name
+    this.paginationLimit = conf.paginationLimit || config.api.DEFAULT_PAGINATION_LIMIT
   }
 
   async getMany(reqQuery: Request['query']) {
@@ -33,64 +39,72 @@ export class Service {
       const key = this._buildListKey(skip, limit)
       let items = await this.cache.get(key)
       if (!items) {
-        items = await this.model.find(reqQuery).skip(skip).limit(limit)
+        items = await this._model.find(reqQuery).skip(skip).limit(limit)
         await this.cache.upsert(items, key)
       }
-      const count = await this.model.count()
+      const count = await this._model.count()
       return { items, count }
     } catch (error) {
+      this.logger.error(`Error on list ${this.name}s`, error)
       return Promise.reject(error)
     }
   }
 
   async getOne(query: GenericObject) {
     try {
-      const key = this._buildGetKey(query)
+      const _query = this._parseQuery(query)
+      const key = this._buildGetKey(_query)
       let item = await this.cache.get(key)
       if (!item) {
-        item = await this.model.findOne(query)
-        if (!item) ApiError.raise.notFound(`${this.conf.name} not found`)
-        await this.cache.upsert(item, query.value)
+        item = await this._model.findOne(_query)
+        if (!item) ApiError.raise.notFound(`${this.name} not found`)
+        await this.cache.upsert(item, key)
       }
       return item
     } catch (error) {
+      this.logger.error(`Error on get ${this.name}`, error)
       return Promise.reject(error)
     }
   }
 
   async insert(data: GenericObject) {
     try {
-      const item = await this.model.create(data)
+      const item = await this._model.create(data)
       return item
     } catch (error) {
+      this.logger.error(`Error on insert ${this.name}`, error)
       return Promise.reject(error)
     }
   }
 
   async update(data: GenericObject, query: GenericObject) {
     try {
-      const item = (await this.model.findOne(query) as GenericDocument)
-      if (!item) ApiError.raise.notFound(`${this.conf.name} not found`)
+      const _query = this._parseQuery(query)
+      const item = (await this._model.findOne(_query) as GenericDocument)
+      if (!item) ApiError.raise.notFound(`${this.name} not found`)
       Object.keys(data).forEach((key) => {
         if (data[key] !== undefined) item[key] = data[key]
       })
       await item.save()
-      this.cache.upsert(item.toJSON(), query.value)
+      this.cache.upsert(item.toJSON(), this._buildGetKey(query))
       return item
     } catch (error) {
+      this.logger.error(`Error on update ${this.name}`, error)
       return Promise.reject(error)
     }
   }
 
   async delete(query: GenericObject) {
     try {
-      const item = await this.model.findOneAndDelete(query)
+      const _query = this._parseQuery(query)
+      const item = await this._model.findOneAndDelete(_query)
       if (item) {
         this.cache.delete(query.value)
         return null
       }
-      ApiError.raise.notFound(`${this.conf.name} not found`)
+      ApiError.raise.notFound(`${this.name} not found`)
     } catch (error) {
+      this.logger.error(`Error on delete ${this.name}`, error)
       return Promise.reject(error)
     }
   }
@@ -98,19 +112,28 @@ export class Service {
   private _getSkipLimit(query: Request['query']) {
     const { skip: _skip, limit: _limit } = query
     const skip = _skip ? Number(_skip) : 0
-    const limit = _limit ? Number(_limit) : this.conf.paginationLimit
+    const limit = _limit ? Number(_limit) : this.paginationLimit
     delete query.skip
     delete query.limit
     return { skip, limit }
   }
 
-  private _buildListKey(skip: number, limit: number): string {
+  protected _parseQuery(query: GenericObject) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (query.hasOwnProperty('id')) {
+      query._id = Types.ObjectId(query.id)
+      delete query.id
+    }
+    return query
+  }
+
+  protected _buildListKey(skip: number, limit: number): string {
     let key = `${skip}`
     if (limit) key += `_${limit}`
     return key
   }
 
-  private _buildGetKey(query: GenericObject): string {
+  protected _buildGetKey(query: GenericObject): string {
     const keys: string[] = []
     Object.keys(query).forEach((_key) => keys.push(_key))
     return keys.join('_')
