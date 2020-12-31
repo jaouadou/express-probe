ef39e1edae7dd991cb7fe30ab35349cf90b605a3
emanuelosva <emanuelosva@gmail.com>
2020-12-31 09:01:02 -0600

    :hammer: move cache to service

--- src/services/cache/service.ts
+++ src/services/cache/service.ts
@@ -7,37 +7,31 @@
  * in a temporal DB served by Redis and
  * return with better performace the disired
  * response.
+ *
+ * The cache service always must be to expose
+ * at minimun 3 expected methods:
+ * - list -> Get cached response of array info
+ * - get -> Get cached response of single object info
+ * - upsert -> Update or insert new info to cache
  */
 
-import { redisClient } from '../db'
-import { promisify } from 'util'
 import { RedisClient } from 'redis'
+import { promisify } from 'util'
+import { cacheClient } from './client'
+import config from '../../config'
 
-class RedisCache {
-  public time: number
-
-  constructor(
-    cacheSeconds = 60,
-    private client: RedisClient = redisClient,
-  ) {
-    this.client = client
-    this.time = cacheSeconds
-  }
-
-  getCache(key: string) {
-    return promisify(this.client.get).bind(this.client)(key)
-  }
-
-  setCache(key: string, data: Record<string, any> | null) {
-    return promisify(this.client.setex).bind(this.client)(key, this.time, JSON.stringify(data))
-  }
-}
-
-export class Cache extends RedisCache {
+export class Cache {
   public collection: string
-  constructor(collection: string, cacheSeconds: number = 60) {
-    super(cacheSeconds)
+  private time: number
+  private client: RedisClient
+
+  constructor(collection: string, conf?: {
+    client?: RedisClient,
+    cacheSeconds?: number,
+  }) {
     this.collection = collection
+    this.time = conf?.cacheSeconds || config.api.CACHE_SECONDS
+    this.client = conf?.client || cacheClient
   }
 
   async list(key: string = this.collection) {
@@ -63,5 +57,13 @@ export class Cache extends RedisCache {
   async delete(key?: string) {
     const _key = key ? `${this.collection}_${key}` : this.collection
     this.setCache(_key, null)
+  }
+
+  private getCache(key: string) {
+    return promisify(this.client.get).bind(this.client)(key)
+  }
+
+  private setCache(key: string, data: Record<string, any> | null) {
+    return promisify(this.client.setex).bind(this.client)(key, this.time, JSON.stringify(data))
   }
 }
